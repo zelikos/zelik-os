5336bb9e79509d3b7ddea2ed421af3d2a0fd367e
Patrick Csikos <pcsikos@zelikos.dev>
2026-08-26 19:21:55 -0400

    fix(overrides): symlink oo7-daemon

--- build_files/main/03-overrides.sh
+++ build_files/main/03-overrides.sh
@@ -11,3 +11,7 @@ for file in fish htop micro nvtop; do
         sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/"$file".desktop
     fi
 done
+
+# Link /usr/lib/oo7-daemon to /usr/bin/oo7-daemon for oo7 PAM module
+# https://github.com/linux-credentials/oo7/issues/516
+ln -s /usr/lib/oo7-daemon /usr/bin/oo7-daemon
