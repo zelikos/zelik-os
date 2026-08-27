85c2f698964cd545c7cee20a59b42d4bd3f58b9a
zelikos <pcsikos@zelikos.dev>
2026-08-27 09:32:41 -0400

    fix(overrides): Hide launchers of more utilities
    Basically, things that don't show up in a full GNOME desktop
    installation on e.g. Fedora, but are present on Arch Linux.

--- build_files/main/03-overrides.sh
+++ build_files/main/03-overrides.sh
@@ -5,8 +5,8 @@ set -ouex pipefail
 
 echo "===Applying overrides==="
 
-# Hide launchers for CLI programs
-for file in fish htop micro nvtop; do
+# Hide launchers for CLI programs and other extras
+for file in avahi-discover bssh bvnc fish htop lstopo micro nvtop qv4l2 qvidcap; do
     if [[ -f "/usr/share/applications/$file.desktop" ]]; then
         sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/"$file".desktop
     fi
