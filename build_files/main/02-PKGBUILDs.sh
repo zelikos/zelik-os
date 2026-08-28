2f5a5be6dfa47068ab52d3f6bd69d04310c4e7aa
zelikos <pcsikos@zelikos.dev>
2026-08-28 19:22:56 -0400

    build: Add PKGBUILDs step to build.sh
    chore: Remove local xdg-terminal-exec script
    
    fix: Remove bats test from xdg-terminal-exec PKGBUILD
    
    It tries to create `/var/home/build` and fails. Rather than try to
    troubleshoot or find a workaround, I've opted to just remove the check
    step from the PKGBUILD, as I've already successfully run it locally and
    have otherwised verified that the PKGBUILD works fine outside of the
    `podman build` environment.
    
    fix: Add temporary build user for running makepkg
    
    makepkg can't run as root, so we add a temporary build user with
    passwordless sudo access to pacman for handling dependencies and
    installation of the built package.

---
+++ build_files/main/02-PKGBUILDs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+
+echo "===Building and installing custom packages==="
+
+pacman -Syu --noconfirm base-devel
+mkdir /tmp/packages/ && cp -r /ctx/build_files/packages/ /tmp/pkgbuilds/
+
+# Add build user to run makepkg, as makepkg cannot be run as root
+useradd --no-create-home --shell=/bin/false build && usermod -L build
+echo "build ALL=(ALL) NOPASSWD: /usr/bin/pacman" >> /etc/sudoers
+chown -Rc build /tmp/pkgbuilds
+
+# makepkg -srfci
+# -s: Installs required dependencies for PKGBUILD
+# -r: Removes build dependencies after successful build
+# -f: Overwrites any existing package in directory
+# -c: Cleans up work files after build
+# -i: Installs package after successful build
+
+# xdg-terminal-exec https://github.com/Vladimir-csp/xdg-terminal-exec/
+runuser -u build -- makepkg -srfci --noconfirm -D /tmp/pkgbuilds/xdg-terminal-exec
+
+# Cleanup
+userdel build
+pacman -Rs --noconfirm base-devel
